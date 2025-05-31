84067610ad2ee38c8f7aa385c577cc7ffec51b2e
LihinduPerera <lihindu.indudunu.perera@gmail.com>
2025-05-31 22:53:56 +0530

    Add image and video rescaling functionality
    - Created a new script `rescale.py` to handle image and video resizing using OpenCV.
    - Implemented a function `rescaleFrame` to resize frames based on a scaling factor.
    - Added functionality to read and display an image and a video, showing both original and resized versions.
    - Included a `changeRes` function for adjusting video capture resolution.
    - Updated requirements file to include necessary dependencies: `opencv-contrib-python` and `caer`.

--- read.py
+++ read.py
@@ -1,3 +1,22 @@
 import cv2 as cv
 
-cv.imread('')
+#Read an image
+# img = cv.imread('Photos/cat_large.jpg')
+
+# cv.imshow('Cat', img)
+
+#read a video
+# capture = cv.VideoCapture(0) #use intergers for capture the camera - 0 for default camera and 1 , 2 ,3 .... those for other cameras
+capture = cv.VideoCapture('Videos/dog.mp4')
+
+while True:
+    isTrue, frame = capture.read() # Read the current frame
+    cv.imshow('video', frame) # Show the current frame
+
+    if cv.waitKey(20) & 0xFF == ord('d'): # Wait for 20 milliseconds and check if 'd' is pressed
+        break
+
+capture.release() # Release the video capture object
+cv.destroyAllWindows() # Close all OpenCV windows
+
+cv.waitKey(0)
